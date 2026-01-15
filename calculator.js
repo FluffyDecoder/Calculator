b1783fb2efe1e13851ad0099447b6d73b785de03
FluffyDecoder <hussnainshafi1357911@gmail.com>
2026-01-15 19:24:55 +0500

    Add Seperate HTML

---
+++ calculator.js
@@ -0,0 +1,18 @@
+        const display = document.getElementById("display");
+        function toDisplay(input) {
+            display.value += input;
+        }
+        function remove() {
+            display.value = "";
+        }
+        function calculate() {
+            try {
+                display.value = math.evaluate(display.value);
+            }
+            catch (error) {
+                display.value = "Error";
+            }
+        }
+        function backSpace(){
+            display.value = display.value.slice(0,-1);
+        }
